84a95f696c8187c50974cab272e529761d4fc690
Pete Corey <petecorey@gmail.com>
2017-06-30 20:16:55 -0400

    Incremented to 0.2.1

--- package.js
+++ package.js
@@ -1,23 +1,20 @@
 Package.describe({
-  name: 'east5th:check-checker',
-  version: '0.2.0',
-  summary: 'An eslint based check checker.',
-  git: 'https://github.com/East5th/check-checker',
-  documentation: 'README.md'
+    name: 'east5th:check-checker',
+    version: '0.2.1',
+    summary: 'An eslint based check checker.',
+    git: 'https://github.com/East5th/check-checker',
+    documentation: 'README.md'
 });
 
 Package.registerBuildPlugin({
-  name: "check-checker",
-  sources: [
-    "lib/rules/checks.js",
-    "lib/check-checker.js"
-  ],
-  npmDependencies: {
-    "eslint": "2.2.0",
-    "babel-eslint": "5.0.0"
-  }
+    name: 'check-checker',
+    sources: ['lib/rules/checks.js', 'lib/check-checker.js'],
+    npmDependencies: {
+        eslint: '2.2.0',
+        'babel-eslint': '5.0.0'
+    }
 });
 
 Package.onUse(function(api) {
-  api.use("isobuild:linter-plugin@1.0.0");
+    api.use('isobuild:linter-plugin@1.0.0');
 });
